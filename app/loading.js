3edfda60f6bd4ca4401054c29eea2cb67cef3a6f
mehedihasanpatowary007 <hasanmdrifat77@gamil.com>
2025-07-10 11:26:46 +0600

    'upgrade'

--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="h-full flex items-center justify-center bg-white dark:bg-zinc-900">
+    <div className="h-full flex items-center justify-center">
       <div className="flex flex-col items-center gap-4">
         {/* Spinning Icon */}
         <Loader2 className="w-10 h-10 animate-spin text-emerald-500" />
